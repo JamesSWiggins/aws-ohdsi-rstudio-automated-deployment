4bdd23ae9f6d7cafd2e88e347be09afb0a0caead
JamesSWiggins <james.s.wiggins@gmail.com>
2018-05-01 12:52:10 -0400

    First commit

--- config_db.sh
+++ config_db.sh
@@ -60,7 +60,7 @@ psql -d OHDSI --host=$RDS_ENDPOINT --port=5432 -U ohdsi_admin_user -a -f postgre
 aws elasticbeanstalk restart-app-server --environment-name $(echo $EB_ENDPOINT | cut -d . -f1)
 
 #Optionally connect R-Studio Instance to the load balancer
-if [ "$RSTUDIO_TARGET_GROUP_ARN" -ne "none" ]; then
+if [ "$RSTUDIO_TARGET_GROUP_ARN" != "none" ]; then
     export EB_ENVIRONMENT=$(echo $EB_ENDPOINT | cut -d . -f1)
     export EB_LB=$(aws elasticbeanstalk describe-environment-resources --environment-name $EB_ENVIRONMENT --query EnvironmentResources.LoadBalancers --output text)
     export EB_LB_LISTENER=$(aws elbv2 describe-listeners --load-balancer-arn $EB_LB --query 'Listeners[0].ListenerArn' --output text)
@@ -71,18 +71,38 @@ fi
 #Run Achilles R script to enabled Data Source visualization
 sed -i 's!REDSHIFT_ENDPOINT!'$REDSHIFT_ENDPOINT'!' achilles.r
 sed -i 's!DATABASE_PASSWORD!'$DATABASE_PASSWORD'!' achilles.r
+
 date > /tmp/rstudio_sparklyr_emr5.tmp
 export MAKE='make -j 8'
 sudo yum install -y xorg-x11-xauth.x86_64 xorg-x11-server-utils.x86_64 xterm libXt libX11-devel libXt-devel libcurl-devel git compat-gmp4 compat-libffi5 openssl-devel
-sudo yum install -y R R-core R-core-devel R-devel libxml2-devel
+sudo yum install R R-core R-core-devel R-devel libxml2-devel -y
 if [ -f /usr/lib64/R/etc/Makeconf.rpmnew ]; then
 sudo cp /usr/lib64/R/etc/Makeconf.rpmnew /usr/lib64/R/etc/Makeconf
 fi
 if [ -f /usr/lib64/R/etc/ldpaths.rpmnew ]; then
 sudo cp /usr/lib64/R/etc/ldpaths.rpmnew /usr/lib64/R/etc/ldpaths
 fi
+
+mkdir /mnt/r-stuff
+cd /mnt/r-stuff
+
+pushd .
+mkdir R-latest
+cd R-latest
+wget https://cran.r-project.org/src/base/R-3/R-3.5.0.tar.gz
+tar -xzf R-3.5.0.tar.gz
 sudo yum install -y gcc gcc-c++ gcc-gfortran readline-devel cairo-devel libpng-devel libjpeg-devel libtiff-devel
+cd R-3*
+./configure --with-readline=yes --enable-R-profiling=no --enable-memory-profiling=no --enable-R-shlib --with-pic --prefix=/usr --with-x --with-libpng --with-jpeglib --with-cairo --enable-R-shlib --with-recommended-packages=yes
+make -j 8
+sudo make install
+sudo su << BASH_SCRIPT
+echo 'export PATH=${!PWD}/bin:$PATH' >> /etc/profile
+BASH_SCRIPT
+popd
+
 sudo sed -i 's/make/make -j 8/g' /usr/lib64/R/etc/Renviron
+
 # set unix environment variables
 sudo su << BASH_SCRIPT
 echo '
@@ -90,6 +110,8 @@ export JAVA_HOME=/etc/alternatives/jre
 ' >> /etc/profile
 BASH_SCRIPT
 sudo sh -c "source /etc/profile"
+
 # fix java binding - R and packages have to be compiled with the same java version as hadoop
 sudo R CMD javareconf
+
 Rscript achilles.r 
